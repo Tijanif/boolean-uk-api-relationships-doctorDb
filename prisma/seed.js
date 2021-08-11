191489fa5cb24d2bf39f7898c8880cd6ba1e9a56
Tijani Fulani <Fulani07@gmail.com>
2021-08-11 18:02:19 +0100

    seeding for appointments

--- prisma/seed.js
+++ prisma/seed.js
@@ -25,6 +25,34 @@ const seedDoctors = [
   },
 ];
 
+const seedAppointments = [
+  {
+    reason: 'broken leg recovery',
+    date: '11 august 2021',
+    practiceName: 'Some GP office',
+  },
+  {
+    reason: 'constipation',
+    date: '11 august 2021',
+    practiceName: 'Some GP office',
+  },
+  {
+    reason: 'ear infection',
+    date: '11 august 2021',
+    practiceName: 'Some GP office',
+  },
+  {
+    reason: 'back pain',
+    date: '11 august 2021',
+    practiceName: 'Some GP office',
+  },
+];
+
+const getRandomNumber = (array) => {
+  const number = Math.floor(Math.random() * array.length);
+  return array[number];
+};
+
 const seed = async () => {
   const arrayOfDoctorPromises = seedDoctors.map(async (doctor) => {
     return await dbSeedClient.doctor.create({
@@ -34,7 +62,27 @@ const seed = async () => {
 
   const createdDoctors = await Promise.all(arrayOfDoctorPromises);
 
-  console.log('Doctors', createdDoctors);
+  const getDoctorsId = createdDoctors.map(({ id }) => id);
+
+  const arrayOfAppointmentPromises = seedAppointments.map(
+    async (appointment) => {
+      return await dbSeedClient.appointment.create({
+        data: {
+          ...appointment,
+          date: new Date(appointment.date).toISOString(),
+          doctor: {
+            connect: {
+              id: parseInt(getRandomNumber(getDoctorsId)),
+            },
+          },
+        },
+      });
+    }
+  );
+
+  const createdAppointments = await Promise.all(arrayOfAppointmentPromises);
+
+  console.log('Doctors', createdDoctors, 'Appointments', createdAppointments);
 };
 
 seed()
